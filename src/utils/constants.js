5d195905b8bcd156efda7e1e4ab5c6c6575ebc3b
Pérola <perolafontanacorchog@gmail.com>
2025-11-20 15:33:33 -0300

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+export const PATH_SPACESHIP_IMAGE = "src/assets/imagens/spaceship.png";
+export const PATH_ENGINE_IMAGE = "src/assets/imagens/engine.png";
+export const PATH_ENGINE_SPRITES = "src/assets/imagens/engine_sprites.png";
+export const PATH_INVADER_IMAGE = "src/assets/imagens/invader.png";
+
+export const NUMBER_STARS = 100;
+export const INITIAL_FRAMES = 8;
+
+export const GameState = {
+    START: "start",
+    PLAYING: "playing",
+    GAME_OVER: "gameOver",
+};
